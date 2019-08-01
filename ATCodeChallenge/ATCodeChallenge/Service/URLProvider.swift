2d8e68e50178a9700cb25a8ccb937b8baeab45c5
Victor Cotrim <cotrim149@gmail.com>
2019-07-31 22:16:29 -0300

    #2 -Adding URL provider class with base url and other urls

---
+++ ATCodeChallenge/ATCodeChallenge/Service/URLProvider.swift
@@ -0,0 +1,20 @@
+//
+//  URLProvider.swift
+//  ATCodeChallenge
+//
+//  Created by Victor de Lima on 31/07/19.
+//  Copyright © 2019 Victor Cotrim. All rights reserved.
+//
+
+import UIKit
+
+class URLProvider: NSObject {
+
+	static let base = "https://api.themoviedb.org/3"
+	
+	static let baseMovies = "\(base)/movie"
+	
+	static let upcomingMovies = "\(baseMovies)/upcoming"
+	
+	
+}
